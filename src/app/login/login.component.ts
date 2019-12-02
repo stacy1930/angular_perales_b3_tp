28d593562ce53392829299379a08d74fe3ba73f6
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2019-12-02 10:53:13 +0100

    models

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import IUser from "../models/IUser";
 
 @Component({
   selector: "app-login",
@@ -10,7 +11,7 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  login: string;
+  login: "Bret";
   mdp: string;
   loginForm: FormGroup;
   loading = false;
@@ -18,31 +19,36 @@ export class LoginComponent implements OnInit {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  ngOnInit() {
-    this.http
-      .get("https://jsonplaceholder.typicode.com/users")
-      .subscribe(value => {
-        console.log(value);
-      });
-
-    console.log("ici");
-  }
+  ngOnInit() {}
 
   connexion() {
     this.loading = true;
 
-    setTimeout(() => {
-      if (this.login === "admin" && this.mdp === "admin") {
-        this.logged = true;
-        this.snackBar.open("Vous êtes connecté", null, {
-          duration: 2000
-        });
-      } else {
-        this.loading = false;
-        this.snackBar.open("Echec", null, {
-          duration: 2000
-        });
-      }
-    }, 2000);
+    this.http
+      .get("https://jsonplaceholder.typicode.com/users?username=" + this.login) // Retourne un observable
+      .subscribe(
+        (value: IUser[]) => {
+          console.log("get: ", this.login, value.length);
+          console.log(value[0].company.name);
+          if (value.length === 1) {
+            this.logged = true;
+            this.snackBar.open("Vous êtes connecté", null, {
+              duration: 2000
+            });
+          } else {
+            this.loading = false;
+            this.snackBar.open("Connexion échoué", null, {
+              duration: 2000
+            });
+          }
+        },
+        (error1: HttpErrorResponse) => {
+          console.log("erreur1", error1);
+          this.loading = false;
+        },
+        () => {
+          this.loading = false;
+        }
+      );
   }
 }
