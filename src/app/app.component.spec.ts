e4b55ab5566bcd9d54139b0a4f27a90e7197464c
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2019-12-09 12:29:23 +0100

    test unitaire

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,12 +24,12 @@ describe("AppComponent", () => {
     expect(app.title).toEqual("Tp1");
   });
 
-  // it("should render title", () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector(".content span").textContent).toContain(
-  //     "tp1 app is running!"
-  //   );
-  // });
+  it("should render title", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector(".content span").textContent).toContain(
+      "tp1 app is running!"
+    );
+  });
 });
