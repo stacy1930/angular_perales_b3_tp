4c03078faab70a6c354cc1b7e8ce15e694764973
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-02-23 13:57:49 +0100

    list

--- src/app/liste-utilisateur/liste-utilisateur.component.spec.ts
+++ src/app/liste-utilisateur/liste-utilisateur.component.spec.ts
@@ -1,15 +1,16 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { MatListModule } from "@angular/material/list";
-
 import { ListeUtilisateurComponent } from "./liste-utilisateur.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("ListeUtilisateurComponent", () => {
   let component: ListeUtilisateurComponent;
   let fixture: ComponentFixture<ListeUtilisateurComponent>;
+  // let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatListModule],
+      imports: [MatListModule, HttpClientTestingModule],
       declarations: [ListeUtilisateurComponent]
     }).compileComponents();
   }));
